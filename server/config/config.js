e844259c9ab28eec6369bcb021efd352982f93ec
dcarazo <dario_carazo@hotmail.com>
2019-09-30 18:19:36 -0300

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = '//mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:tjmf6b7UUfLCfp56@cluster0-8ogyj.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
